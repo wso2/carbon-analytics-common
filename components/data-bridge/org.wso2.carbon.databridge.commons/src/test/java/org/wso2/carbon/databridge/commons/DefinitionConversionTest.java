4b2edd9a6b4c883c2fe0df5b573fc67bd33e26a9
Ilia Choly <icholy@accipiterradar.com>
2016-05-02 15:00:16 -0400

    Update stream definition test to exercise OBJECT attribute

--- components/data-bridge/org.wso2.carbon.databridge.commons/src/test/java/org/wso2/carbon/databridge/commons/DefinitionConversionTest.java
+++ components/data-bridge/org.wso2.carbon.databridge.commons/src/test/java/org/wso2/carbon/databridge/commons/DefinitionConversionTest.java
@@ -53,7 +53,8 @@ public class DefinitionConversionTest {
                             "          {'name':'price','type':'double'}," +
                             "          {'name':'volume','type':'int'}," +
                             "          {'name':'maxTemp','type':'double'}," +
-                            "          {'name':'minTemp','type':'double'}" +
+                            "          {'name':'minTemp','type':'double'}," +
+                            "          {'name':'rawData','type':'object'}" +
                             "  ]" +
                             "}";
 
@@ -73,6 +74,7 @@ public class DefinitionConversionTest {
         payload.add(new Attribute("volume", AttributeType.INT));
         payload.add(new Attribute("maxTemp", AttributeType.DOUBLE));
         payload.add(new Attribute("minTemp", AttributeType.DOUBLE));
+        payload.add(new Attribute("rawData", AttributeType.OBJECT));
         streamDefinition2.setPayloadData(payload);
 
         Assert.assertEquals(streamDefinition1, streamDefinition2);
