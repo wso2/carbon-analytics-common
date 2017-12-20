48c97587f3f169eec121a3894572f163e0631f4c
Niveathika <niveathika@wso2.com>
2017-12-20 11:10:06 +0530

    Changed to parameterized log messages

--- components/authentication/org.wso2.carbon.analytics.idp.client/src/main/java/org/wso2/carbon/analytics/idp/client/local/LocalIdPClient.java
+++ components/authentication/org.wso2.carbon.analytics.idp.client/src/main/java/org/wso2/carbon/analytics/idp/client/local/LocalIdPClient.java
@@ -92,7 +92,7 @@ public class LocalIdPClient implements IdPClient {
         if (user != null) {
             return user.getRoles();
         }
-        LOG.debug("User with username '" + name + "' is not present when retrieving user roles.");
+        LOG.debug("User with username '{}' is not present when retrieving user roles.", name);
         return new ArrayList<>();
     }
 
@@ -121,7 +121,7 @@ public class LocalIdPClient implements IdPClient {
                         sessionIdSessionMap.replace(oldSession.getSessionId().toString(), oldSession);
                         refreshIdSessionMap.replace(oldSession.getRefreshId().toString(), oldSession);
 
-                        LOG.debug("User '" + userName + "' session is extended.");
+                        LOG.debug("User '{}' session is extended.", userName);
                         returnProperties.put(IdPClientConstants.LOGIN_STATUS,
                                 IdPClientConstants.LoginStatus.LOGIN_SUCCESS);
                         returnProperties.put(IdPClientConstants.USERNAME, userName);
@@ -153,8 +153,8 @@ public class LocalIdPClient implements IdPClient {
                     Arrays.fill(byteBuffer.array(), (byte) 0); // clear sensitive data
 
                     if (!Arrays.equals(plainUserPassword, password.getBytes(Charset.forName("UTF-8")))) {
-                        LOG.debug("Password did not match with the configured user, userName: '" +
-                                userName + "', Failing the authentication.");
+                        LOG.debug("Password did not match with the configured user, userName: '{}', " +
+                                "Failing the authentication.", userName);
                         returnProperties.put(IdPClientConstants.LOGIN_STATUS,
                                 IdPClientConstants.LoginStatus.LOGIN_FAILURE);
                         returnProperties.put(IdPClientConstants.ERROR, IdPClientConstants.Error.INVALID_CREDENTIALS);
@@ -174,7 +174,7 @@ public class LocalIdPClient implements IdPClient {
                         sessionIdSessionMap.put(session.getSessionId().toString(), session);
                         refreshIdSessionMap.put(session.getRefreshId().toString(), session);
 
-                        LOG.debug("User '" + userName + "' is logged in.");
+                        LOG.debug("User '{}' is logged in.", userName);
                         returnProperties.put(IdPClientConstants.LOGIN_STATUS,
                                 IdPClientConstants.LoginStatus.LOGIN_SUCCESS);
                         returnProperties.put(IdPClientConstants.USERNAME, userName);
@@ -186,7 +186,7 @@ public class LocalIdPClient implements IdPClient {
                         return returnProperties;
                     }
                 } else {
-                    LOG.debug("User not found for userName: '" + userName + "'. Failing the authentication.");
+                    LOG.debug("User not found for userName: '{}'. Failing the authentication.", userName);
                     returnProperties.put(IdPClientConstants.LOGIN_STATUS, IdPClientConstants.LoginStatus.LOGIN_FAILURE);
                     returnProperties.put(IdPClientConstants.ERROR, IdPClientConstants.Error.INVALID_CREDENTIALS);
                     returnProperties.put(IdPClientConstants.ERROR_DESCRIPTION, "The login credential used for " +
@@ -210,7 +210,7 @@ public class LocalIdPClient implements IdPClient {
                         sessionIdSessionMap.put(refreshSession.getSessionId().toString(), refreshSession);
                         refreshIdSessionMap.put(refreshSession.getRefreshId().toString(), refreshSession);
 
-                        LOG.debug("User '" + refreshSession.getUsername() + "' session is refreshed.");
+                        LOG.debug("User '{}' session is refreshed.", refreshSession.getUsername());
                         returnProperties.put(IdPClientConstants.LOGIN_STATUS,
                                 IdPClientConstants.LoginStatus.LOGIN_SUCCESS);
                         returnProperties.put(IdPClientConstants.USERNAME, refreshSession.getUsername());
