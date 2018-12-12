d828947fc12e8779491af5d6ddee8540ad593475
lasanthaS <lasn1987@gmail.com>
2018-12-12 12:33:09 +0530

    Add timeout to testReceiverConnection test case

--- components/data-bridge/org.wso2.carbon.databridge.receiver.binary/src/test/java/org.wso2.carbon.databridge.receiver.binary.test/BinaryDataReceiverTest.java
+++ components/data-bridge/org.wso2.carbon.databridge.receiver.binary/src/test/java/org.wso2.carbon.databridge.receiver.binary.test/BinaryDataReceiverTest.java
@@ -134,6 +134,7 @@ public class BinaryDataReceiverTest {
 
         BinaryDataReceiver binaryDataReceiver = new BinaryDataReceiver(dataReceiverConfiguration, dataBridge);
         binaryDataReceiver.start();
+        Thread.sleep(1000);
         SocketAddress tcpSockAddress = new InetSocketAddress("127.0.0.1", 9611);
         SocketAddress sslSockAddress = new InetSocketAddress("127.0.0.1", 9711);
         Socket socketTCP = new Socket();
