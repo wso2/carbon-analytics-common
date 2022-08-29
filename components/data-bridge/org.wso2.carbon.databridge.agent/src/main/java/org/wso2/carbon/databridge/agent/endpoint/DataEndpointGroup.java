c98e13ff8e8522973561d47f8ddf065948a759e9
binodmx <binod@wso2.com>
2022-08-29 16:10:06 +0530

    Remove unnecessary isEPCheckedGap check

--- components/data-bridge/org.wso2.carbon.databridge.agent/src/main/java/org/wso2/carbon/databridge/agent/endpoint/DataEndpointGroup.java
+++ components/data-bridge/org.wso2.carbon.databridge.agent/src/main/java/org/wso2/carbon/databridge/agent/endpoint/DataEndpointGroup.java
@@ -438,8 +438,6 @@ public class DataEndpointGroup implements DataEndpointFailureCallback {
     private class ReconnectionTask extends TimerTask {
         public void run() {
             long gap = 0;
-            long isEPCheckedGap = 0;
-            long addedDelayForEPCheck = 10000;
             for (int i = START_INDEX; i < maximumDataPublisherIndex.get(); i++) {
                 DataEndpoint dataEndpoint = dataEndpoints.get(i);
                 if (!dataEndpoint.isConnected()) {
@@ -453,13 +451,10 @@ public class DataEndpointGroup implements DataEndpointFailureCallback {
                     }
                 } else {
                     try {
-                        if (System.currentTimeMillis() > isEPCheckedGap) {
-                            isEPCheckedGap = System.currentTimeMillis() + addedDelayForEPCheck;
-                            String[] urlElements = DataPublisherUtil.getProtocolHostPort(
-                                    dataEndpoint.getDataEndpointConfiguration().getReceiverURL());
-                            if (!isServerExists(urlElements[1], Integer.parseInt(urlElements[2]))) {
-                                dataEndpoint.deactivate();
-                            }
+                        String[] urlElements = DataPublisherUtil.getProtocolHostPort(
+                                dataEndpoint.getDataEndpointConfiguration().getReceiverURL());
+                        if (!isServerExists(urlElements[1], Integer.parseInt(urlElements[2]))) {
+                            dataEndpoint.deactivate();
                         }
                     } catch (DataEndpointConfigurationException exception) {
                         log.warn("Data Endpoint with receiver URL:" +
