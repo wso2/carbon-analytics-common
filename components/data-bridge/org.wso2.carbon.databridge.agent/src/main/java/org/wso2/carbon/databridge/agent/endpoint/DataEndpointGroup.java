d5b38ef4a5afc20d8eec64c8ef4d194d7037753b
binodmx <binod@wso2.com>
2022-08-29 14:11:39 +0530

    Fix CPU spike in ReconnectionTask

--- components/data-bridge/org.wso2.carbon.databridge.agent/src/main/java/org/wso2/carbon/databridge/agent/endpoint/DataEndpointGroup.java
+++ components/data-bridge/org.wso2.carbon.databridge.agent/src/main/java/org/wso2/carbon/databridge/agent/endpoint/DataEndpointGroup.java
@@ -42,6 +42,8 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -103,8 +105,8 @@ public class DataEndpointGroup implements DataEndpointFailureCallback {
 
         currentDataPublisherIndex.set(START_INDEX);
         ReconnectionTask reconnectionTask = new ReconnectionTask();
-        reconnection = new Thread(reconnectionTask);
-        reconnection.start();
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(reconnectionTask, 0, 30000);
     }
 
     public void addDataEndpoint(DataEndpoint dataEndpoint) {
@@ -433,38 +435,36 @@ public class DataEndpointGroup implements DataEndpointFailureCallback {
         return unsuccessfulEvents;
     }
 
-    private class ReconnectionTask implements Runnable {
+    private class ReconnectionTask extends TimerTask {
         public void run() {
             long gap = 0;
             long isEPCheckedGap = 0;
-            long addedDelayForEPCheck = 10000l;
-            while (true) {
-                for (int i = START_INDEX; i < maximumDataPublisherIndex.get(); i++) {
-                    DataEndpoint dataEndpoint = dataEndpoints.get(i);
-                    if (!dataEndpoint.isConnected()) {
-                        gap = System.currentTimeMillis() - dataEndpoint.getReConnectTimestamp();
-                        try {
-                            if (gap > 0) {
-                                dataEndpoint.connect();
-                            }
-                        } catch (Exception ex) {
-                            dataEndpoint.deactivate();
+            long addedDelayForEPCheck = 10000;
+            for (int i = START_INDEX; i < maximumDataPublisherIndex.get(); i++) {
+                DataEndpoint dataEndpoint = dataEndpoints.get(i);
+                if (!dataEndpoint.isConnected()) {
+                    gap = System.currentTimeMillis() - dataEndpoint.getReConnectTimestamp();
+                    try {
+                        if (gap > 0) {
+                            dataEndpoint.connect();
                         }
-                    } else {
-                        try {
-                            if (System.currentTimeMillis() > isEPCheckedGap) {
-                                isEPCheckedGap = System.currentTimeMillis() + addedDelayForEPCheck;
-                                String[] urlElements = DataPublisherUtil.getProtocolHostPort(
-                                        dataEndpoint.getDataEndpointConfiguration().getReceiverURL());
-                                if (!isServerExists(urlElements[1], Integer.parseInt(urlElements[2]))) {
-                                    dataEndpoint.deactivate();
-                                }
+                    } catch (Exception ex) {
+                        dataEndpoint.deactivate();
+                    }
+                } else {
+                    try {
+                        if (System.currentTimeMillis() > isEPCheckedGap) {
+                            isEPCheckedGap = System.currentTimeMillis() + addedDelayForEPCheck;
+                            String[] urlElements = DataPublisherUtil.getProtocolHostPort(
+                                    dataEndpoint.getDataEndpointConfiguration().getReceiverURL());
+                            if (!isServerExists(urlElements[1], Integer.parseInt(urlElements[2]))) {
+                                dataEndpoint.deactivate();
                             }
-                        } catch (DataEndpointConfigurationException exception) {
-                            log.warn("Data Endpoint with receiver URL:" +
-                                    dataEndpoint.getDataEndpointConfiguration().getReceiverURL()
-                                    + " could not be deactivated", exception);
                         }
+                    } catch (DataEndpointConfigurationException exception) {
+                        log.warn("Data Endpoint with receiver URL:" +
+                                dataEndpoint.getDataEndpointConfiguration().getReceiverURL()
+                                + " could not be deactivated", exception);
                     }
                 }
             }
