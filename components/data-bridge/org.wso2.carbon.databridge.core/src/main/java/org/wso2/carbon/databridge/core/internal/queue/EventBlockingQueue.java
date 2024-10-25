4994afb73cf99dfcd537755997fc021cb1443ea6
Ashan Rathnaweera <ashanahr@gmail.com>
2024-10-25 10:25:24 +0530

    Restructure the polling comparison logic

--- components/data-bridge/org.wso2.carbon.databridge.core/src/main/java/org/wso2/carbon/databridge/core/internal/queue/EventBlockingQueue.java
+++ components/data-bridge/org.wso2.carbon.databridge.core/src/main/java/org/wso2/carbon/databridge/core/internal/queue/EventBlockingQueue.java
@@ -81,9 +81,9 @@ public class EventBlockingQueue extends ArrayBlockingQueue<EventComposite> {
     public EventComposite poll() {
         EventComposite eventComposite = super.poll();
         currentSize.addAndGet(-eventComposite.getSize());
-        if (semaphore.availablePermits() == 0 && ((currentEventCompositeSize + currentSize.get()) < maxSize) || isEmpty()) {
+        if (semaphore.availablePermits() == 0 && (((currentEventCompositeSize + currentSize.get()) < maxSize) || isEmpty())) {
             synchronized (lock) {
-                if (semaphore.availablePermits() == 0 && ((currentEventCompositeSize + currentSize.get()) < maxSize) || isEmpty()) {
+                if (semaphore.availablePermits() == 0 && (((currentEventCompositeSize + currentSize.get()) < maxSize) || isEmpty())) {
                     semaphore.release();
                 }
             }
